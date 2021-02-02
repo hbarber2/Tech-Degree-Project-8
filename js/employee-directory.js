95cc573ab3ca859436581c6421885e68018e7c17
hbarber2 <hbarber295@gmail.com>
2021-02-02 10:27:08 -0500

    Commit 3

--- js/employee-directory.js
+++ js/employee-directory.js
@@ -9,90 +9,65 @@ let employees = [];
 function fetchRandomUser(url) {
 	fetch(url)
 		.then(response => response.json())
-		.then(userJSON => {
-			for(i = 0; i < 12; i++){
-					generateEmployeeProfile(userJSON.results[i], i)
-					employees.push(userJSON.results[i])
-			}
-		})
-		.catch(e => console.log(e));
+		.then(userJSON => userJSON.results)
+		.then(generateEmployeeProfile)
+		.catch(e => console.log(e))
 };
 
 
-function generateEmployeeProfile(json, index) {
-	const profile = document.createElement('div');
-	employeeProfilesContainer.appendChild(profile);
-	profile.className = 'employee-info';
-	profile.innerHTML = 
+function generateEmployeeProfile(json) {
+	
+	employees = json;
+	
+	employees.forEach((employee, index) => {
+		let thumbnail = employee.picture.thumbnail								
+		let employeeName = employee.name.first + ' ' + employee.name.last
+		let email = employee.email
+		let city = employee.location.city
+		const profile = document.createElement('div');
+		employeeProfilesContainer.appendChild(profile);
+		profile.className = 'employee-info';
+		profile.innerHTML = 
 			`
-			<img src=${json.picture.thumbnail}>
-			<h3 class="employee-name">${json.name.first} ${json.name.last}</h3>
-			<p class="email">${json.email}</p>
-			<p class="city">${json.location.city}</p>
+			<img src=${thumbnail}>
+			<h3 class="employee-name">${employeeName}</h3>
+			<p class="email">${email}</p>
+			<p class="city">${city}</p>
 			`;
+	
+
+
 	
 		profile.addEventListener('click', () => {
 			let profileImg = document.getElementById('profile-pic');
 			let name = document.getElementById('employee-name');
 			let address = document.getElementById('employee-address');
-			let city = document.getElementById('employee-city');
-			let email = document.getElementById('employee-email');
+			let employeeCity = document.getElementById('employee-city');
+			let employeeEmail = document.getElementById('employee-email');
 			let dob = document.getElementById('employee-birthday');
 			let cellPhone = document.getElementById('employee-cell-phone');
-			let employee = employees[index];
 			let birthday = new Date(employees[index].dob.date);
 
 			overlay.classList.remove('hidden');
 			profileImg.src = employee.picture.large
 			name.innerHTML = employee.name.first + ' ' + employee.name.last;
-			city.innerHTML = employee.location.city
+			employeeCity.innerHTML = employee.location.city
 			address.innerHTML = employee.location.street.number + ' ' + employee.location.street.name + ' ' + employee.location.city + ', ' + employee.location.state + ' ' + employee.location.postcode;
-			email.innerHTML = employee.email
+			employeeEmail.innerHTML = employee.email
 			dob.innerHTML = birthday.getMonth() + '/' + birthday.getDate() + '/' + birthday.getFullYear()
 			cellPhone.innerHTML = employee.cell
-	})
-	
-};
+				});
 
-closeButton.addEventListener('click', () => {
-	overlay.classList.add('hidden');
-});
+		});
+	};
 
+
+		closeButton.addEventListener('click', () => {
+			overlay.classList.add('hidden');
+		});
 
 
 fetchRandomUser(randomUser);
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-
-
 
 
 
